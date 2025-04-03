d11be1ebe1953508710428eccc946bc0c7bb51fa
IngenieroJosser <cordobarivasjosser@gmail.com>
2025-04-02 23:56:18 -0500

    feat(whatsapp): whatsApp API for talking to customers

--- src/components/ui/home/Advantages.tsx
+++ src/components/ui/home/Advantages.tsx
@@ -1,22 +1,22 @@
 const Advantages = () => {
   const advantages = [
     {
-      title: "Atención al Cliente",
-      description: "Nuestro equipo dedicado ofrece asistencia oportuna, comprometido a resolver tus dudas.",
-      linkText: "Contáctanos",
-      linkHref: "#",
+      title: 'Atención al Cliente',
+      description: 'Nuestro equipo dedicado ofrece asistencia oportuna, comprometido a resolver tus dudas.',
+      linkText: 'Contáctanos',
+      linkHref: 'https://api.whatsapp.com/send?phone=+573232842193&text=%C2%A1Hola!%20En%20*Choc%C3%B3Market*%20ofrecemos%20productos%20aut%C3%A9nticos%20del%20Choc%C3%B3%2C%20conectando%20a%20productores%20locales%20con%20el%20mundo.%20%C2%BFEn%20qu%C3%A9%20podemos%20ayudarte%3F',
     },
     {
-      title: "Satisfacción Garantizada",
-      description: "Invierte en nuestros productos con total confianza, respaldado por nuestra garantía de satisfacción de 30 días.",
-      linkText: "Aprende más",
-      linkHref: "#",
+      title: 'Satisfacción Garantizada',
+      description: 'Invierte en nuestros productos con total confianza, respaldado por nuestra garantía de satisfacción de 30 días.',
+      linkText: 'Aprende más',
+      linkHref: 'https://api.whatsapp.com/send?phone=+573232842193&text=%C2%A1Hola!%20En%20*Choc%C3%B3Market*%20ofrecemos%20productos%20aut%C3%A9nticos%20del%20Choc%C3%B3%2C%20conectando%20a%20productores%20locales%20con%20el%20mundo.%20%C2%BFEn%20qu%C3%A9%20podemos%20ayudarte%3F',
     },
     {
-      title: "Envío Gratis",
-      description: "Disfruta de una experiencia de compra sin interrupciones, facilitada por nuestro servicio de envío gratuito.",
-      linkText: "Aprende más",
-      linkHref: "#",
+      title: 'Envío Gratis',
+      description: 'Disfruta de una experiencia de compra sin interrupciones, facilitada por nuestro servicio de envío gratuito.',
+      linkText: 'Aprende más',
+      linkHref: 'https://api.whatsapp.com/send?phone=+573232842193&text=%C2%A1Hola!%20En%20*Choc%C3%B3Market*%20ofrecemos%20productos%20aut%C3%A9nticos%20del%20Choc%C3%B3%2C%20conectando%20a%20productores%20locales%20con%20el%20mundo.%20%C2%BFEn%20qu%C3%A9%20podemos%20ayudarte%3F',
     },
   ];
 
