01204232e07628886c35827f87ebd4128b3ca61c
Joss <133165583+IngenieroJosser@users.noreply.github.com>
2025-04-03 10:19:44 -0500

    feat(ui): placing a component (Header) on the admin screen

--- src/app/admin-dashboard/page.tsx
+++ src/app/admin-dashboard/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { AuthRoute } from '@/components/AuthRoute';
+import Header from '@/components/ui/home/Header';
 
 export default function AdminDashboard() {
   return (
     <>
       <AuthRoute allowedRoles={['ADMIN']} />
       {/* Contenido del dashboard para el administrador*/}
+      <Header />
       <p>Gestión del administrador</p>
     </>
   );
