f9098e585f286e8c553efc5639bee370a2a22560
Joss <133165583+IngenieroJosser@users.noreply.github.com>
2025-04-29 15:24:03 -0500

    feat: create the componente ShoppingCart

--- src/components/ui/home/Header.tsx
+++ src/components/ui/home/Header.tsx
@@ -6,6 +6,7 @@ import { Select } from "../../Select";
 import { useDarkMode } from "@/hooks/useDarkMode";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
+import ShoppingCart from "@/components/ShoppingCart";
 
 const Header = () => {
   const [language, setLanguage] = useState<string>("es");
@@ -87,23 +88,16 @@ const Header = () => {
 
   const toggleCart = () => setIsCartOpen(!isCartOpen);
 
-  const removeItem = (itemId: number) => {
-    setCartItems(cartItems.filter((item) => item.id !== itemId));
-  };
-
   const handleLogout = () => {
-    // Limpiar localStorage
     localStorage.removeItem("token");
     localStorage.removeItem("userRole");
     localStorage.removeItem("userName");
 
-    // Actualizar estado
     setIsLoggedIn(false);
     setUserName("Usuario");
 
-    // Redirigir y recargar el estado
     router.push("/");
-    router.refresh(); // Forzar actualización de la página
+    router.refresh();
   };
 
   return (
@@ -494,66 +488,7 @@ const Header = () => {
               <p className="text-center text-gray-500">Tu carrito está vacío</p>
             ) : (
               <>
-                <div className="space-y-4">
-                  {cartItems.map((item) => (
-                    <div key={item.id} className="border-b pb-4">
-                      <div className="flex justify-between items-start">
-                        <div>
-                          <Image
-                            src={item.image}
-                            width={200}
-                            height={200}
-                            alt={item.name}
-                          />
-                          <h3 className="font-semibold uppercase">
-                            {item.name}
-                          </h3>
-                          <p className="text-sm text-gray-500">
-                            Color: {item.color}
-                          </p>
-                          <p className="text-lg">
-                            ${item.price.toFixed(2)} CAD
-                          </p>
-                        </div>
-                        <button
-                          aria-label="Botón para eliminar algunos productos del carrito de compras"
-                          onClick={() => removeItem(item.id)}
-                          className="bg-red-600 border-b-2 p-3 text-white hover:font-bold text-sm cursor-pointer transition-all duration-300"
-                        >
-                          Eliminar
-                        </button>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-
-                <div className="mt-8">
-                  <div className="flex justify-between font-semibold text-lg mb-4">
-                    <span>Subtotal:</span>
-                    <span>
-                      $
-                      {cartItems
-                        .reduce((sum, item) => sum + item.price, 0)
-                        .toFixed(2)}{" "}
-                      COP
-                    </span>
-                  </div>
-                  <p className="text-sm text-gray-500 mb-6">
-                    Impuestos incluidos y envío calculado al finalizar
-                  </p>
-
-                  <button aria-label="Botón para finalizar compras" className="w-full bg-[#008060] cursor-pointer text-white py-3 rounded hover:bg-[#00694d] transition-colors">
-                    Finalizar Compra
-                  </button>
-
-                  <button
-                    aria-label="Botón para seguir comprando"
-                    onClick={toggleCart}
-                    className="w-full mt-4 text-[#008060] cursor-pointer hover:text-[#00694d] underline"
-                  >
-                    Seguir comprando
-                  </button>
-                </div>
+                <ShoppingCart />
               </>
             )}
           </motion.div>
