036cfab232acb6259084fcb07dfa2161c16a0626
Joss <133165583+IngenieroJosser@users.noreply.github.com>
2025-05-08 17:47:19 -0500

    something

--- src/components/ui/shop/ProductGrid.tsx
+++ src/components/ui/shop/ProductGrid.tsx
@@ -40,7 +40,7 @@ const ProductGrid = ({ products = [] }: ProductGridProps) => {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-      {shuffledProducts.map((product) => {
+      {shuffledProducts.map((product) => { 
         const discountPrice = product.discount
           ? product.price * (1 - product.discount)
           : product.price;
