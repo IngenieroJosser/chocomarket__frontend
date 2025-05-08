de058d82196ea14ecc3befa720fa855a7afe945a
Joss <133165583+IngenieroJosser@users.noreply.github.com>
2025-05-08 16:11:56 -0500

    style(header): change text color for better readability

--- src/components/ui/home/Header.tsx
+++ src/components/ui/home/Header.tsx
@@ -89,7 +89,7 @@ const Header = () => {
             ? ""
             : scrolled
             ? "bg-[rgba(255,255,255,0.56)] dark:bg-[rgba(0,0,0,0.5)] backdrop-blur-md"
-            : "bg-[rgba(255,255,255,0.4)]"
+            : "text-white font-bold"
         }`}
       >
         {/* Logo */}
