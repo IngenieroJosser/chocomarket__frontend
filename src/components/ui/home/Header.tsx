42bb3cf8f1c5714d11d8cd3e61d57ea18b3b3401
Joss <133165583+IngenieroJosser@users.noreply.github.com>
2025-05-08 16:15:56 -0500

    style(header): I leave it as it was before

--- src/components/ui/home/Header.tsx
+++ src/components/ui/home/Header.tsx
@@ -89,7 +89,7 @@ const Header = () => {
             ? ""
             : scrolled
             ? "bg-[rgba(255,255,255,0.56)] dark:bg-[rgba(0,0,0,0.5)] backdrop-blur-md"
-            : "text-white font-bold"
+            : ""
         }`}
       >
         {/* Logo */}
